58f627cd95c9fb9140a9c34302dc229aadde55d1
pedabir <i.makhonkov@hotmail.com>
2018-05-04 21:31:14 +0300

    add logic of game/ style/ corrected timer

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ var ctx = canvas.getContext('2d');
 var mas = [];
 var count = 0;
 var timer;
-var clickHandler = 0;
 // вешаем событие для отрисовки бактерий 
 canvas.onclick = function (event) {
     var x = event.offsetX;
@@ -27,18 +26,17 @@ function gameField() {
 }
 gameField();
 
-
 // рисуем прямоугольник
 function drawField() {
     ctx.clearRect(0, 0, 800 , 800);
      for(var i=0; i<80; i++) {
         for (var j=0; j<80; j++){
             if (mas[i][j]==1) {
-                ctx.fillStyle = "grey";
-                ctx.fillRect(j*10, i*10, 10, 10);
+                ctx.strokeStyle = "white";
+                ctx.strokeRect(j*10, i*10, 10, 10);
             }
         }
-     }
+    }
 }
 
 
@@ -49,6 +47,7 @@ function createLife() {
         mas2[i]=[];
         for (var j=0; j<80; j++){
             var neighbors = 0;
+            var state = mas[i][j];
             // подсчет количества соседей
             if (mas[minus(i)-1][j]==1) neighbors++; // верх
             if (mas[i][plus(j)+1]==1) neighbors++;// право
@@ -58,11 +57,13 @@ function createLife() {
             if (mas[plus(i)+1][plus(j)+1]==1) neighbors++; // правый низ 
             if (mas[plus(i)+1][minus(j)-1]==1) neighbors++; // левый низ
             if (mas[minus(i)-1][minus(j)-1]==1) neighbors++; // левый верх
-            // условия жизни
-            else if (neighbors==2 || neighbors==3) {
-                mas2[i][j]=1;
-            } else if (neighbors > 3 || neighbors < 2) {
-                    mas2[i][j]==0;
+            //правила жизни
+            if (state == 0 && neighbors == 3) {
+                    mas2[i][j] = 1;
+               }else if (state == 1 && neighbors < 2 || neighbors > 3) {
+                    mas2[i][j] = 0; 
+               } else {
+                    mas2[i][j] = state;
                 }
             }
         }
@@ -91,15 +92,13 @@ document.getElementById("start").onclick = function() {
 }
 
 document.getElementById("pause").onclick = function() {
-
         clearTimeout(timer);
-
 }
 
 document.getElementById("clear").onclick = function() {
     document.getElementById('count').innerHTML = 0;
+    count = 0;
     clearTimeout(timer);
     ctx.clearRect(0, 0, 800 , 800);
-    mas = [];
     gameField();
 }
